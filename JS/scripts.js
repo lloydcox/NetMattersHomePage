3c4a66e219e92b180cea0ac20546b297d3dc0458
Lloyd <lloydcox13@aol.com>
2020-08-13 06:55:43 +0100

    JavaScript updates
    Menu button and cookies mainly.

--- JS/scripts.js
+++ JS/scripts.js
@@ -14,33 +14,65 @@ $(document).ready(function () {
     });
 
 // Cookie Scripts
-
-if($('.cookie-banner').length) {
-    $('.cookie-banner').slideDown(800);
-}
-
- 
-
+$(document).ready(function () {
+$(".grt-cookies").grtCookie({
+    textcolor: "#333",
+    background: "#fff",
+    buttonbackground: "#333",
+    buttontextcolor: "#fff",
+  });
+});
 // Side Menu scripts
-
-var	menuRight = document.getElementById( 'cbp-spmenu-s2' ),
-    showRightPush = document.getElementById( 'showRightPush' ),
+$(document).ready(function () {
+var menuRight = document.getElementById('cbp-spmenu-s2'),
+    showRightPush = document.getElementById('showRightPush'),
+    crossRightPush = document.getElementById('crossRightPush1'),
     body = document.body;
 
-showRightPush.onclick = function() {
-	classie.toggle( this, 'active' );
-	classie.toggle( body, 'cbp-spmenu-push-toleft' );
-	classie.toggle( menuRight, 'cbp-spmenu-open' );
-	disableOther( 'showRightPush' );
+
+showRightPush.onclick = function () {
+    "use strict";
+    classie.toggle(this, 'active');
+    classie.toggle(body, 'cbp-spmenu-push-toleft');
+    classie.toggle(menuRight, 'cbp-spmenu-open');
+    disableOther('showRightPush');
+    if (showRightPush.style.display != 'none') {
+        crossRightPush.style.display = 'none';
+        showRightPush.style.display = 'flex';
+      } else {
+        crossRightPush.style.display = 'none';
+        showRightPush.style.display = 'flex';
+      }
 };
 
-function disableOther( button ) {
+crossRightPush.onclick = function () {
+    "use strict";
+    classie.toggle(this, 'active');
+    classie.toggle(body, 'cbp-spmenu-push-toleft');
+    classie.toggle(menuRight, 'cbp-spmenu-open');
+    disableOther('crossRightPush');
+    disableOther1('showRightPush');
+    if (crossRightPush.style.display != 'none') {
+        showRightPush.style.display = 'none';
+        crossRightPush.style.display = 'flex';
+      } else {
+        showRightPush.style.display = 'none';
+        crossRightPush.style.display = 'flex';
+      }
+};
 
-	if( button !== 'showRightPush' ) {
-		classie.toggle( showRightPush, 'disabled' );
-	}
+function disableOther(button) {
+    "use strict";
+    if (button !== 'showRightPush') {
+        classie.toggle(showRightPush, 'disabled');
+    }
 }
 
+function disableOther1(button) {
+    "use strict";
+    if (button !== 'crossRightPush') {
+        classie.toggle(crossRightPush, 'disabled');
+    }
+}
 
-});
-
+});'end'});
