6b55a9b9e132c69ba91772838819d98fbd15721a
Lloyd <lloydcox13@aol.com>
2020-08-24 15:17:25 +0100

    Major JS updates
    Updates to Cookie, scroll nav bar and more.

--- JS/scripts.js
+++ JS/scripts.js
@@ -14,65 +14,87 @@ $(document).ready(function () {
     });
 
 // Cookie Scripts
-$(document).ready(function () {
-$(".grt-cookies").grtCookie({
-    textcolor: "#333",
-    background: "#fff",
-    buttonbackground: "#333",
-    buttontextcolor: "#fff",
-  });
-});
+const modal = document.querySelector('.modal');
+const acceptBtn = document.querySelector('#acceptCookies');
+
+
+/** Show Modal **/
+(() => {
+  const isCookieAccepted = JSON.parse(window.localStorage.getItem('cookie'));
+
+  if (isCookieAccepted) {
+    console.log(`Cookie status: ${isCookieAccepted}`)
+  } else {
+    modal.classList.add('show')
+  }
+})();
+
+/** Close modal and Set cookie true in ls **/
+acceptBtn.addEventListener('click', () => {
+  window.localStorage.setItem('cookie', true);
+  modal.classList.remove('show')
+})
+
+
+
 // Side Menu scripts
-$(document).ready(function () {
-var menuRight = document.getElementById('cbp-spmenu-s2'),
+
+$(document).ready(function() {
+  var menuRight = document.getElementById('cbp-spmenu-s2'),
     showRightPush = document.getElementById('showRightPush'),
     crossRightPush = document.getElementById('crossRightPush1'),
     body = document.body;
-
-
-showRightPush.onclick = function () {
-    "use strict";
+  
+  
+  showRightPush.onclick = function() {
     classie.toggle(this, 'active');
     classie.toggle(body, 'cbp-spmenu-push-toleft');
     classie.toggle(menuRight, 'cbp-spmenu-open');
     disableOther('showRightPush');
     if (showRightPush.style.display != 'none') {
-        crossRightPush.style.display = 'none';
-        showRightPush.style.display = 'flex';
-      } else {
-        crossRightPush.style.display = 'none';
-        showRightPush.style.display = 'flex';
-      }
-};
-
-crossRightPush.onclick = function () {
-    "use strict";
+      crossRightPush.style.display = 'none';
+    } else {
+      crossRightPush.style.display = 'none';
+      showRightPush.style.display = 'flex';
+    }
+  };
+  
+  crossRightPush.onclick = function() {
     classie.toggle(this, 'active');
     classie.toggle(body, 'cbp-spmenu-push-toleft');
     classie.toggle(menuRight, 'cbp-spmenu-open');
     disableOther('crossRightPush');
-    disableOther1('showRightPush');
     if (crossRightPush.style.display != 'none') {
-        showRightPush.style.display = 'none';
-        crossRightPush.style.display = 'flex';
-      } else {
-        showRightPush.style.display = 'none';
-        crossRightPush.style.display = 'flex';
-      }
-};
-
-function disableOther(button) {
-    "use strict";
+      showRightPush.style.display = 'none';
+    } else {
+      showRightPush.style.display = 'none';
+      crossRightPush.style.display = 'flex';
+    }
+  };
+  
+  function disableOther(button) {
     if (button !== 'showRightPush') {
-        classie.toggle(showRightPush, 'disabled');
+      classie.toggle(showRightPush, 'disabled');
     }
-}
-
-function disableOther1(button) {
-    "use strict";
+  }
+  
+  function disableOther(button) {
     if (button !== 'crossRightPush') {
-        classie.toggle(crossRightPush, 'disabled');
+      classie.toggle(crossRightPush, 'disabled');
     }
-}
+  }
+  })
+  });
 
-});'end'});
+// Nav Bar 
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("scroll_nav").style.top = "0";
+  } else {
+    document.getElementById("scroll_nav").style.top = "-250px";
+  }
+  prevScrollpos = currentScrollPos;
+}
